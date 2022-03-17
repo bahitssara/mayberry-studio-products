7e0dacfc73a75ccb009fbb198b5b0f50eee4aa6a
Sara Mayberry <saraamayberry@yahoo.com>
2022-03-17 17:45:11 -0600

    gsap additions/mobile

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 (() => {
   // testing JS file readability
+    const mobileNav = document.getElementById('mobile-nav');
 
     function homePageAnimations(){
-      gsap.fromTo(".home-page__intro--copy", {opacity: 0}, {opacity:1, duration: 5})
+      gsap.fromTo('.home-page__intro--copy', {opacity: 0}, {opacity:1, duration: 5});
     }
 
+    function navToggle(){
+      const navLinks = document.getElementById('navigation-links');
+      if(navLinks.classList.contains('active')) {
+        navLinks.classList.remove('active');
+        gsap.to('#navigation-links', {right: -400, duration: 1, ease: Power1.easeOut});
+      } else {
+        navLinks.classList.add('active');
+        gsap.to('#navigation-links', {right: 0, duration: 1,ease: Power1.easeOut});
+      }
+    }
+
+    mobileNav.addEventListener('click', navToggle);
 
 
     //excecute functions
     homePageAnimations();
+    // navToggle();
 })();
